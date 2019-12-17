b6f8537a1a00858d38417711b6673cac111c408d
Marcelo Y. Matuda <src@mym.eng.br>
2019-12-17 15:50:33 -0300

    Matrix: Rename interval to range

--- src/util/Matrix.h
+++ src/util/Matrix.h
@@ -1,6 +1,6 @@
 /*
 
-  Copyright (c) 2013, 2017, 2018, 2019 Marcelo Y. Matuda.
+  Copyright (c) 2014, 2017, 2018, 2019 Marcelo Y. Matuda.
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
@@ -33,6 +33,7 @@
 #define MATRIX_H_
 
 #include <algorithm> /* swap */
+#include <initializer_list>
 #include <limits>
 #include <ostream>
 #include <vector>
@@ -55,113 +56,125 @@ public:
 	typedef const T& ConstReference;
 	typedef T ValueType;
 
+	template<typename U>
+	class Range {
+	public:
+		Range(U begin, U end) : begin_(begin), end_(end) {}
+		U& begin() { return begin_; }
+		U& end() { return end_; }
+		U& begin() const { return begin_; }
+		U& end() const { return end_; }
+	private:
+		U begin_;
+		U end_;
+	};
+
+	template<typename U>
 	class Dim1Iterator {
 	public:
-		Dim1Iterator(typename Matrix<T, Alloc>::Pointer baseValuePtr, typename Matrix<T, Alloc>::SizeType increment)
+		Dim1Iterator(U* baseValuePtr, typename Matrix<T, Alloc>::SizeType increment)
 			: valuePtr_(baseValuePtr)
 			, increment_(increment) {
 		}
-		Dim1Iterator(const Dim1Iterator& iter)
+		Dim1Iterator(const Dim1Iterator<U>& iter)
 			: valuePtr_(iter.valuePtr_)
 			, increment_(iter.increment_) {
 		}
-		Dim1Iterator& operator=(const Dim1Iterator& iter) {
+		Dim1Iterator& operator=(const Dim1Iterator<U>& iter) {
 			if (&iter != this) {
 				valuePtr_ = iter.valuePtr_;
 				increment_ = iter.increment_;
 			}
 			return *this;
 		}
-		bool operator==(const Dim1Iterator& iter) const { return !(valuePtr_ != iter.valuePtr_ || increment_ != iter.increment_); }
-		bool operator!=(const Dim1Iterator& iter) const { return valuePtr_ != iter.valuePtr_ || increment_ != iter.increment_; }
-		Dim1Iterator& operator++() { valuePtr_ += increment_; return *this; }
-		Dim1Iterator operator++(int) { Dim1Iterator iter{*this}; valuePtr_ += increment_; return iter; }
-		typename Matrix<T, Alloc>::Reference operator*() { return *valuePtr_; }
-	private:
-		typename Matrix<T, Alloc>::Pointer valuePtr_;
-		const typename Matrix<T, Alloc>::SizeType increment_;
-	};
+		~Dim1Iterator() = default;
 
-	class ConstDim1Iterator {
-	public:
-		ConstDim1Iterator(typename Matrix<T, Alloc>::ConstPointer baseValuePtr, typename Matrix<T, Alloc>::SizeType increment)
-			: valuePtr_(baseValuePtr)
-			, increment_(increment) {
+		bool operator==(const Dim1Iterator<U>& iter) const {
+			return valuePtr_ == iter.valuePtr_ && increment_ == iter.increment_;
 		}
-		ConstDim1Iterator(const ConstDim1Iterator& iter)
-			: valuePtr_(iter.valuePtr_)
-			, increment_(iter.increment_) {
+		bool operator!=(const Dim1Iterator<U>& iter) const {
+			return !(*this == iter);
 		}
-		ConstDim1Iterator& operator=(const ConstDim1Iterator& iter) {
-			if (&iter != this) {
-				valuePtr_ = iter.valuePtr_;
-				increment_ = iter.increment_;
-			}
+		Dim1Iterator& operator++() {
+			valuePtr_ += increment_;
 			return *this;
 		}
-		bool operator==(const ConstDim1Iterator& iter) const { return !(valuePtr_ != iter.valuePtr_ || increment_ != iter.increment_); }
-		bool operator!=(const ConstDim1Iterator& iter) const { return valuePtr_ != iter.valuePtr_ || increment_ != iter.increment_; }
-		ConstDim1Iterator& operator++() { valuePtr_ += increment_; return *this; }
-		ConstDim1Iterator operator++(int) { ConstDim1Iterator iter{*this}; valuePtr_ += increment_; return iter; }
-		typename Matrix<T, Alloc>::ConstReference operator*() { return *valuePtr_; }
+		Dim1Iterator operator++(int) {
+			Dim1Iterator<U> iter{*this};
+			valuePtr_ += increment_;
+			return iter;
+		}
+		U& operator*() const {
+			return *valuePtr_;
+		}
 	private:
-		typename Matrix<T, Alloc>::ConstPointer valuePtr_;
+		Dim1Iterator(Dim1Iterator<U>&&) = delete;
+		Dim1Iterator& operator=(Dim1Iterator<U>&&) = delete;
+
+		U* valuePtr_;
 		const typename Matrix<T, Alloc>::SizeType increment_;
 	};
-
-	typedef std::pair<Dim1Iterator, Dim1Iterator> Dim1Interval;
-	typedef std::pair<ConstDim1Iterator, ConstDim1Iterator> ConstDim1Interval;
 
 	typedef Iterator Dim2Iterator;
 	typedef ConstIterator ConstDim2Iterator;
-	typedef std::pair<Dim2Iterator, Dim2Iterator> Dim2Interval;
-	typedef std::pair<ConstDim2Iterator, ConstDim2Iterator> ConstDim2Interval;
 
 	Matrix();
 	Matrix(SizeType n1, SizeType n2);
+	Matrix(std::initializer_list<std::vector<T>> il);
 
 	SizeType size() const { return data_.size(); }
 	SizeType n1() const { return n1_; }
 	SizeType n2() const { return n2_; }
 
-	Reference operator()(SizeType dim1, SizeType dim2);
-	ConstReference operator()(SizeType dim1, SizeType dim2) const;
+	Reference operator()(SizeType i1, SizeType i2);
+	ConstReference operator()(SizeType i1, SizeType i2) const;
 
-	Dim1Interval dim1Interval(SizeType dim2) {
-		Pointer valuePtr = &data_[dim2];
-		return std::make_pair(Dim1Iterator(valuePtr, n2_), Dim1Iterator(valuePtr + n1_ * n2_, n2_));
+	Range<Dim1Iterator<T>> range1(SizeType i2) {
+		Pointer valuePtr = &data_[i2];
+		return Range<Dim1Iterator<T>>(
+					Dim1Iterator<T>(valuePtr            , n2_),
+					Dim1Iterator<T>(valuePtr + n1_ * n2_, n2_));
+	}
+	Range<Dim1Iterator<const T>> range1(SizeType i2) const {
+		ConstPointer valuePtr = &data_[i2];
+		return Range<Dim1Iterator<const T>>(
+					Dim1Iterator<const T>(valuePtr            , n2_),
+					Dim1Iterator<const T>(valuePtr + n1_ * n2_, n2_));
 	}
 
-	ConstDim1Interval dim1Interval(SizeType dim2) const {
-		ConstPointer valuePtr = &data_[dim2];
-		return std::make_pair(ConstDim1Iterator(valuePtr, n2_), ConstDim1Iterator(valuePtr + n1_ * n2_ , n2_));
+	Range<Dim2Iterator> range2(SizeType i1) {
+		auto baseIter = data_.begin() + i1 * n2_;
+		return Range<Dim2Iterator>(baseIter, baseIter + n2_);
 	}
-
-	Dim2Interval dim2Interval(SizeType dim1) {
-		Iterator baseIter = data_.begin() + dim1 * n2_;
-		return std::make_pair(baseIter, baseIter + n2_);
-	}
-
-	ConstDim2Interval dim2Interval(SizeType dim1) const {
-		ConstIterator baseIter = data_.begin() + dim1 * n2_;
-		return std::make_pair(baseIter, baseIter + n2_);
+	Range<ConstDim2Iterator> range2(SizeType i1) const {
+		auto baseIter = data_.cbegin() + i1 * n2_;
+		return Range<ConstDim2Iterator>(baseIter, baseIter + n2_);
 	}
 
 	void resize(SizeType n1, SizeType n2);
 	void reset();
 	void operator=(T value);
-	bool empty() const { return data_.size() == 0; }
+	bool empty() const { return data_.empty(); }
 	void swap(Matrix& other);
 
 	Iterator begin() { return data_.begin(); }
 	Iterator end() { return data_.end(); }
 	ConstIterator begin() const { return data_.begin(); }
 	ConstIterator end() const { return data_.end(); }
+	ConstIterator cbegin() const { return data_.cbegin(); }
+	ConstIterator cend() const { return data_.cend(); }
+
+	bool operator==(const Matrix<T, Alloc>& m) const {
+		return n1_ == m.n1_ && n2_ == m.n2_ && data_ == m.data_;
+	}
+	bool operator!=(const Matrix<T, Alloc>& m) const {
+		return !(*this == m);
+	}
 private:
 	template<typename V>
 	friend std::ostream& operator<<(std::ostream& out, const Matrix<V>& m);
 
-	void validateSize(SizeType n1, SizeType n2);
+	static void validateSize(SizeType n1, SizeType n2);
 
 	SizeType n1_;
 	SizeType n2_;
@@ -177,22 +190,38 @@ template<typename T, typename Alloc>
 Matrix<T, Alloc>::Matrix(SizeType n1, SizeType n2) : n1_(n1), n2_(n2)
 {
 	validateSize(n1, n2);
-
 	data_.resize(n1 * n2);
 }
 
 template<typename T, typename Alloc>
-typename Matrix<T, Alloc>::Reference
-Matrix<T, Alloc>::operator()(SizeType dim1, SizeType dim2)
+Matrix<T, Alloc>::Matrix(std::initializer_list<std::vector<T>> il)
 {
-	return data_[dim1 * n2_ + dim2];
+	n1_ = il.size();
+	if (n1_ == 0) {
+		n2_ = 0;
+		return;
+	}
+	n2_ = il.begin()->size();
+	if (n2_ == 0) THROW_EXCEPTION(InvalidValueException, "Empty row.");
+	validateSize(n1_, n2_);
+	for (auto p = il.begin(); p != il.end(); ++p) {
+		if (p->size() != n2_) THROW_EXCEPTION(InvalidValueException, "Inconsistent row size.");
+		data_.insert(data_.end(), p->begin(), p->end());
+	}
+}
+
+template<typename T, typename Alloc>
+typename Matrix<T, Alloc>::Reference
+Matrix<T, Alloc>::operator()(SizeType i1, SizeType i2)
+{
+	return data_[i1 * n2_ + i2];
 }
 
 template<typename T, typename Alloc>
 typename Matrix<T, Alloc>::ConstReference
-Matrix<T, Alloc>::operator()(SizeType dim1, SizeType dim2) const
+Matrix<T, Alloc>::operator()(SizeType i1, SizeType i2) const
 {
-	return data_[dim1 * n2_ + dim2];
+	return data_[i1 * n2_ + i2];
 }
 
 template<typename T, typename Alloc>
@@ -256,9 +285,9 @@ std::ostream&
 operator<<(std::ostream& out, const Matrix<T, Alloc>& m)
 {
 	for (typename Matrix<T, Alloc>::SizeType i = 0, n1 = m.n1(); i < n1; ++i) {
-		typename Matrix<T, Alloc>::ConstDim2Interval dim2Interval = m.dim2Interval(i);
-		for (typename Matrix<T, Alloc>::ConstDim2Iterator iter = dim2Interval.first; iter != dim2Interval.second; ++iter) {
-			out << ' ' << *iter;
+		auto range = m.range2(i);
+		for (auto it = range.begin(); it != range.end(); ++it) {
+			out << ' ' << *it;
 		}
 		out << '\n';
 	}
